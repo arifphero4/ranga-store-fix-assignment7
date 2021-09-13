a5a8d217338797b6a1e233f9cdcddd0fdc3d2aba
arifphero <ahammadarif232@gmail.com>
2021-09-13 17:15:10 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.image;
+    const images = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${images}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
